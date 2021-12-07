9ae1323ff3fbc50f01685600d17b3a5202133ff4
Will <w.young@schaetz-cro.ch>
2021-12-07 09:13:10 +0000

    couch_util:priv_dir

--- src/couch/src/couch_ejson_compare.erl
+++ src/couch/src/couch_ejson_compare.erl
@@ -16,11 +16,13 @@
 
 -export([less/2, less_json_ids/2, less_json/2]).
 
+-include_lib("couch/include/couch_db.hrl").
+
 -on_load(init/0).
 
 init() ->
     NumScheds = erlang:system_info(schedulers),
-    Dir = code:priv_dir(couch),
+    Dir = couch_util:priv_dir(),
     ?assertEqual(Dir, ?MODULE),
     ok = erlang:load_nif(filename:join(Dir, ?MODULE), NumScheds).
 
